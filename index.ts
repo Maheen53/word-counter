073426c809dc53dd05eb541159137b375e5c0187
“[firstname <maheenimtiaz536@gmail.com>
2024-04-17 14:40:05 -0700

    word counter project

---
+++ index.ts
@@ -0,0 +1,25 @@
+#! usr/bin/env node
+// import the inquirer
+import inquirer from "inquirer"
+
+// declare a const `answer` and assign it to the result of inquirer.prompt function
+
+const answers:{
+    Sentence: string
+} = await  inquirer.prompt([
+    {
+        name:"Sentence",
+        type:"input",
+        message:"enter your Sentence to count the word "
+    }
+])
+
+const  words =answers.Sentence.trim().split(" ")
+
+// print the array to the word to console
+
+console.log(words)
+
+// print the word count of the sentence to console
+
+console.log(`your sentence word count is ${words.length}`)
